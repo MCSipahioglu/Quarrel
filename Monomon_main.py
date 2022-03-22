a3777beeb5591e036f72e68836309d73180bd86d
MCSipahioglu <81450182+MCSipahioglu@users.noreply.github.com>
2022-03-22 23:53:41 +0300

    HP hesabı yanlış, word list aynı değil, yoksa her şey OK

--- Monomon_main.py
+++ Monomon_main.py
@@ -20,65 +20,70 @@ driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()),optio
 driver.get("https://squabble.me/")      #Access the Site First
 print("Site Accessed.")
 
-button_SquabbleRoyale=WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, "/html/body/div/div[1]/div[2]/div/div[2]")))   #Squaable Royale button                                 #The button is found very fast but takes time to be clickable, wait for it.
+button_SquabbleRoyale=WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, "/html/body/div/div[1]/div[2]/div/div[2]")))    #Squaable Royale button
 button_SquabbleRoyale.click()                  #Click Squabble Royale by XPath
 print("Squabble Royale Selected.")
 
-WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, "/html/body/div/div[1]/div[2]/div/div[3]/div/input")))    #Wait until Join Game has spawned (i.e. Next page has loaded)
+WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, "/html/body/div/div[1]/div[2]/div/div[3]/div/input")))                #Wait until Join Game has spawned (i.e. Next page has loaded)
 time.sleep(1)
 
-button_FindGame      =WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, "/html/body/div/div[1]/div[2]/div/div[4]")))   #Find Game button
+button_FindGame      =WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, "/html/body/div/div[1]/div[2]/div/div[4]")))    #Find Game button
 button_FindGame.click() #Click Find Game
 print("Find Game Initiated.")
 
-input=WebDriverWait(driver, 300).until(EC.presence_of_element_located((By.XPATH, "/html/body/div[1]/div[1]/div/div[2]/div[1]/div[9]/input")))    #Wait until specific input region has spawned (i.e. Game Has Started) up to 5 minutes
+input=WebDriverWait(driver, 300).until(EC.presence_of_element_located((By.XPATH, "/html/body/div[1]/div[1]/div/div[2]/div[1]/div[9]/input")))   #Wait until specific input region has spawned (i.e. Game Has Started) up to 5 minutes
 time.sleep(1)
 print("Game Started!")
 
-end=0
-while end!=1 :
-    possible_words=Monomon_solver.possible_words    #Reset possible words to all possible words
-    row=3                                           #Due to html organization
-    result=""                                       #Random, in order to enter the loop.
-    guess_counter=1
+try:
+    end=0
+    while end!=1 :
+        HP_string=driver.find_element(By.XPATH,"/html/body/div[1]/div[1]/div/div[2]/div[1]/div[1]").get_attribute("innerText")
+        HP=int(HP_string[11:len(HP_string)-3])
+        print(HP)
 
-    while result != "ggggg" and guess_counter<6:
-        
-        #Guess
-        guess = Monomon_solver.bestWord(possible_words, Monomon_solver.letterFreq(possible_words))
-        input.send_keys(guess)        #to the search element send the string by typing it with keys
-        input.send_keys(Keys.RETURN)    #Press enter at the search element
-        print(f'Guess= {guess.upper()}')
-        time.sleep(1)   #Allow the result to be updated.
+        possible_words=Monomon_solver.possible_words    #Reset possible words to all possible words
+        row=3                                           #Due to html organization
+        result=""                                       #Random, in order to enter the loop.
+        guess_counter=0
 
-        #Result
-        result=""
-        for i in range (1,6):
-            letter_element=driver.find_element(By.XPATH, f"/html/body/div[1]/div[1]/div/div[2]/div[1]/div[{row}]/div[{i}]/div[2]")
-            letter_class=letter_element.get_attribute("class")
-            if(letter_class=="MuiBox-root css-18037ny"):
-                result+="w"
-            elif(letter_class=="MuiBox-root css-tim17a"):
-                result+="y"
-            else:
-                result+="g"
-        print(f'Result={result.upper()}')
-        row+=1
-        guess_counter+=1
+        while result != "ggggg" and guess_counter<6:
+            
+            #Guess
+            guess = Monomon_solver.bestWord(possible_words, Monomon_solver.letterFreq(possible_words))
+            input.send_keys(guess)        #to the search element send the string by typing it with keys
+            input.send_keys(Keys.RETURN)    #Press enter at the search element
+            #print(f'Guess= {guess.upper()}')
+            time.sleep(1)   #Allow the result to be updated.
 
-        #Update Possible Words Left
-        possible_words = Monomon_solver.word_remover(result, guess, possible_words)
-        print(possible_words)
-        if len(possible_words) == 0:
-            break
+            #Result
+            result=""
+            for i in range (1,6):
+                letter_element=driver.find_element(By.XPATH, f"/html/body/div[1]/div[1]/div/div[2]/div[1]/div[{row}]/div[{i}]/div[2]")
+                letter_class=letter_element.get_attribute("class")
+                if(letter_class=="MuiBox-root css-18037ny"):
+                    result+="w"
+                elif(letter_class=="MuiBox-root css-tim17a"):
+                    result+="y"
+                else:
+                    result+="g"
+            #print(f'Result={result.upper()}')
+            row+=1
+            guess_counter+=1
+
+            if len(possible_words)<10:
+                print(f"Possible Words Left: {possible_words}")
+            
 
 
-time.sleep(100)
-
-
-#Bazı sözcükler eksik olabilir.
-#End game condition önemli, HP'ye bakalım
-#Do for second loop as well.
-#Export Python as exe
-#Reconnecting olunca napcaz
-
+            #Update Possible Words Left
+            possible_words = Monomon_solver.word_remover(result, guess, possible_words)
+            #print(possible_words)
+            if len(possible_words) == 0:
+                break
+except:
+    time.sleep(100)
+#Export Python as exe /html/body/div/div[1]/div/div[2]/div[1]/div[1]
+#Reconnecting olunca napcaz delayı arttır.
+#Bazen false ggggg buluyo
+#Bazen possible words leftten değil başka yerden guessliyor. Eksik kelimeler var bizim dictionaryde bence ondan. İmkansız oluyo
